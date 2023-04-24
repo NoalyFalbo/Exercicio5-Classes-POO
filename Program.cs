8ce7edceed789e87ddeb3a52558f3b6111151c87
Noaly Falbo <noalyfalbos@outlook.com>
2023-04-24 14:10:22 -0300

    versão atualizada

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace exercicioClasses5
+{
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+
+      Console.Clear();
+
+      Retangulo ret = new Retangulo();
+
+      Console.WriteLine("Digite a largura do retnângulo:");
+      ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      Console.WriteLine("Digite a altura do retângulo: ");
+      ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+      Console.WriteLine("Area = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+      Console.WriteLine("Perimentro = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+      Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+    }
+  }
+}
